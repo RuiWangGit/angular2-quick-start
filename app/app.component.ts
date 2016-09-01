850c0aebcffe941f678d944410f5519c0d119a28
Rui Wang <wangr3@gmail.com>
2016-09-01 11:43:19 -0700

    change template to templateUrl

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My Second Angular 2 zApp</h1>'
+    //template: '<h1>My First Angular 2 App</h1>'
+    //template: `<h1>{{title}}</h1>
+    //           <div [style.color]="wardsColor">Hello {{name}}</div>
+    //           <button (click)="changeSuitColor()">Change Suit Color</button>
+    //`
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+
+export class AppComponent {
+  // [] means property binding
+  // () means event binding
+  title = 'Custom App';
+  name = "Ward";
+  wardsColor = "red";
+
+  changeSuitColor() {
+    this.wardsColor =  this.wardsColor === 'blue' ? 'red': 'blue';
+  }
+
+}
