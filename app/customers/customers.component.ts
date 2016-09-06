162bec9af636934feb203c1b0429997c7bd7fedb
Rui Wang <wangr3@gmail.com>
2016-09-06 15:23:13 -0700

    observable to array, working version

--- app/customers/customers.component.ts
+++ app/customers/customers.component.ts
@@ -12,27 +12,38 @@ import { Observable } from 'rxjs/Rx'
     providers: [CustomerService]
 })
 export class CustomersComponent implements OnInit{
-  customers: Promise<any[]>;
+  //customers: Promise<any[]>;
   //customers: Observable<any[]>;
-  //customers: any[];
+  customers: any[];
 
   constructor(private _customerService: CustomerService) {console.log(module.id); }
 
 
   ngOnInit() {
 
+
+    //Observale to array
+    this._customerService.getCustomers_RxObservable()
+      .subscribe(
+        (customers) => this.customers = customers,
+        (err) => { console.log(err) }
+      )
+
+
+
+    //Straight up promise to array
     //this._customerService.getCustomers()
     //  .then((customers) => { this.customers = customers; })
     //  .catch( (err) => { console.log(err);  })
     //
 
 
-    //Promise<any[]> version
-    this.customers = this._customerService.getCustomers()
-      .catch( (err) => {
-        console.log(err);  //don't do this, show the user a nice message
-        // now we eat it, but only if the message has been communicated to the user
-      })
+    ////Promise<any[]> version
+    //this.customers = this._customerService.getCustomers()
+    //  .catch( (err) => {
+    //    console.log(err);  //don't do this, show the user a nice message
+    //    // now we eat it, but only if the message has been communicated to the user
+    //  })
 
     //Rx observable version
     //this.customers = this._customerService.getCustomers()
