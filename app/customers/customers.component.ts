61a335dd19c0cd7e6bc5f960db7b3834fb04d1f6
Rui Wang <wangr3@gmail.com>
2016-09-06 15:15:30 -0700

    customer promise

--- app/customers/customers.component.ts
+++ app/customers/customers.component.ts
@@ -21,18 +21,18 @@ export class CustomersComponent implements OnInit{
 
   ngOnInit() {
 
-    this._customerService.getCustomers()
-      .then((customers) => { this.customers = customers; })
-      .catch( (err) => { console.log(err);  })
-
+    //this._customerService.getCustomers()
+    //  .then((customers) => { this.customers = customers; })
+    //  .catch( (err) => { console.log(err);  })
+    //
 
 
     //Promise<any[]> version
-    //this.customers = this._customerService.getCustomers()
-    //  .catch( (err) => {
-    //    console.log(err);  //don't do this, show the user a nice message
-    //    // now we eat it, but only if the message has been communicated to the user
-    //  })
+    this.customers = this._customerService.getCustomers()
+      .catch( (err) => {
+        console.log(err);  //don't do this, show the user a nice message
+        // now we eat it, but only if the message has been communicated to the user
+      })
 
     //Rx observable version
     //this.customers = this._customerService.getCustomers()
